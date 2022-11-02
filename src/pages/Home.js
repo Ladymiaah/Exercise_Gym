6159b4af737b2662ea73c77e8fa57e4bd1b72762
Adedoyin Lawal <adedoyinlawal819@gmail.com>
2022-11-02 14:48:10 +0100

    gym commit

---
+++ src/pages/Home.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+import { Box } from '@mui/material';
+
+import Exercises from '../components/Exercises';
+import SearchExercise from '../components/SearchExercise'; 
+import HeroBanner from '../components/HeroBanner'; 
+
+
+const Home = () => {
+  return (
+   <Box>
+    <HeroBanner/>
+    <SearchExercise/>
+    <Exercises/>
+   </Box>
+  )
+}
+
+export default Home
